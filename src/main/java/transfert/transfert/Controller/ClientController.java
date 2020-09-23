f1e44917375e34f83eb1bb22d51b6382769a405b
OusmaneMballo <ousmanemballo576@gmail.com>
2020-09-23 12:36:37 +0000

    add and listing clients

--- src/main/java/transfert/transfert/Controller/ClientController.java
+++ src/main/java/transfert/transfert/Controller/ClientController.java
@@ -3,8 +3,12 @@ package transfert.transfert.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import transfert.transfert.DAO.ClientRepository;
+import transfert.transfert.Model.Client;
 
 @Controller
 @RequestMapping("/client")
@@ -13,7 +17,19 @@ public class ClientController {
     ClientRepository clientRepos;
     @RequestMapping("/accueil")
     public String index(Model model){
-        model.addAttribute("client", clientRepos.findAll());
+        model.addAttribute("clients", clientRepos.findAll());
         return "client/index";
+    }
+
+    @PostMapping("/add")
+    public String add(@Validated Client client, BindingResult result){
+        if(!(result.hasErrors())){
+            try{
+                clientRepos.save(client);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return "redirect:/client/accueil";
     }
 }
